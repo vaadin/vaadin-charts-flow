aa834d85d196f90bbad2e87523db731d7fe983a6
Johannes Häyry <johannes.hayry@vaadin.com>
2017-11-10 13:54:11 +0200

    Fix wrongly merged version of MainView

--- integration-test/src/main/java/com/vaadin/addon/charts/ui/MainView.java
+++ integration-test/src/main/java/com/vaadin/addon/charts/ui/MainView.java
@@ -1,66 +1,83 @@
 /*
- * Copyright 2000-2017 Vaadin Ltd.
+ * #%L
+ * Vaadin Charts
+ * %%
+ * Copyright (C) 2014 Vaadin Ltd
+ * %%
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * See the file licensing.txt distributed with this software for more
+ * information about licensing.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * You should have received a copy of the CVALv3 along with this program.
+ * If not, see <https://vaadin.com/license/cval-3>.
+ * #L%
  */
 package com.vaadin.addon.charts.ui;
 
-import com.vaadin.addon.charts.Chart;
-import com.vaadin.addon.charts.model.ChartType;
-import com.vaadin.addon.charts.model.Configuration;
-import com.vaadin.addon.charts.model.ListSeries;
-import com.vaadin.addon.charts.model.XAxis;
-import com.vaadin.addon.charts.model.YAxis;
+import java.util.Optional;
+
+import com.vaadin.addon.charts.AbstractChartExample;
+import com.vaadin.addon.charts.examples.area.AreaChart;
+import com.vaadin.router.HasUrlParameter;
 import com.vaadin.router.Route;
-import com.vaadin.router.RouterLayout;
-import com.vaadin.ui.Composite;
+import com.vaadin.router.WildcardParameter;
+import com.vaadin.router.event.BeforeNavigationEvent;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.common.StyleSheet;
 import com.vaadin.ui.html.Div;
-import com.vaadin.ui.html.NativeButton;
 
 @StyleSheet("context://styles.css")
 @Route("")
-public class MainView extends Composite<Div> implements RouterLayout {
+public class MainView extends Div implements HasUrlParameter<String> {
+    public static String EXAMPLE_BASE_PACKAGE = "com.vaadin.addon.charts.examples.";
 
-	public MainView() {
-		final Chart chart = new Chart();
+    @Override
+    public void setParameter(BeforeNavigationEvent event,
+            @WildcardParameter String parameter) {
+        removeAll();
+        Optional<Component> content = getContentFromParameter(parameter);
+        if (content.isPresent()) {
+            add(content.get());
+        } else {
+            setText("couldn't find demo for url: " + parameter);
+        }
+    }
 
-		Configuration configuration = chart.getConfiguration();
-		configuration.setTitle("First Chart for Flow");
-		chart.getConfiguration().getChart().setType(ChartType.AREA);
-		getContent().add(chart);
+    /**
+     * Parses route parameter to obtain the actual example to show
+     * 
+     * @param route
+     *            path that can be either
+     *            <ul>
+     *            <li>category&#47demo</li>
+     *            <li>fully qualified name</li>
+     *            </ul>
+     * 
+     * @return Component for route or {@link Optional#empty()}
+     */
+    private Optional<Component> getContentFromParameter(String route) {
+        // Empty route will show a simple chart by default otherwise parameter
+        // will be converted to full qualified class and will be instantiated
+        if (route == null || route.isEmpty()) {
+            return Optional.of(new AreaChart());
+        }
+        String className;
+        if (route.startsWith(EXAMPLE_BASE_PACKAGE)) {
+            className = route;
+        } else {
+            className = EXAMPLE_BASE_PACKAGE + route.replace("/", ".");
+        }
+        try {
+            @SuppressWarnings("unchecked")
+            Class<? extends AbstractChartExample> forName = (Class<? extends AbstractChartExample>) Class
+                    .forName(className);
+            return Optional.of(forName.newInstance());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
 
-		configuration.addSeries(new ListSeries("Tokyo", 20, 12,34,23,65,8,4,7,76, 19, 20,8));
-		configuration.addSeries(new ListSeries("Miami", 34, 29,23,65,8,4,7,7, 59,8, 9, 19));
-
-		XAxis x = new XAxis();
-		x.setCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
-				"Sep", "Oct", "Nov", "Dec");
-		configuration.addxAxis(x);
-
-		YAxis y = new YAxis();
-		y.setMin(0);
-		y.setTitle("Rainfall (mm)");
-		configuration.addyAxis(y);
-
-		NativeButton changeTitleButton = new NativeButton();
-		changeTitleButton.setId("change_title");
-		changeTitleButton.setText("Change title");
-		changeTitleButton.addClickListener(e -> {
-			configuration.setTitle("First Chart for Flow - title changed");
-			chart.drawChart();
-		});
-
-		getContent().add(changeTitleButton);
-	}
 }
