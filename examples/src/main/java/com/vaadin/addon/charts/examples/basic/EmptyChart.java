df6030f41b0361dd4c30be3575b3967c1a980dc6
Guillermo Alvarez <guillermo@vaadin.com>
2017-11-22 17:47:26 +0200

    Remove empty chart from demo
    Set a new demo by default

--- examples/src/main/java/com/vaadin/addon/charts/examples/basic/EmptyChart.java
+++ examples/src/main/java/com/vaadin/addon/charts/examples/basic/EmptyChart.java
@@ -1,8 +1,10 @@
 package com.vaadin.addon.charts.examples.basic;
 
-import com.vaadin.addon.charts.Chart;
 import com.vaadin.addon.charts.AbstractChartExample;
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.SkipFromDemo;
 
+@SkipFromDemo
 public class EmptyChart extends AbstractChartExample {
 
     @Override
