82e534e12abb096c40b2b9a494d6acda09076f1a
Sayo Oladeji <sayo@vaadin.com>
2018-01-15 15:43:19 +0200

    Removed manual sauce-connect installation

--- integration-test/lib/sauce-connect-installer.sh
+++
@@ -1,3 +0,0 @@
-echo "INSTALLING SAUCE CONNECT LOCALLY"
-mvn install:install-file -Dfile=lib/sauce-connect-3.1.32.jar -DpomFile=lib/sauce-connect-3.1.32.pom
-echo "SAUCE CONNECT INSTALLATION SUCCESSFUL!"
