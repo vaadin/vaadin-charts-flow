2dc4ecd0bca0986e8e459717d814a63d7eee1641
Sayo Oladeji <sayo@vaadin.com>
2018-01-15 12:19:36 +0200

    Circumvent need for unix executable permission for sauce-connect installer

--- integration-test/lib/sauce-connect-installer.sh
+++ integration-test/lib/sauce-connect-installer.sh
@@ -1,3 +1,3 @@
 echo "INSTALLING SAUCE CONNECT LOCALLY"
-mvn install:install-file -Dfile=sauce-connect-3.1.32.jar -DpomFile=sauce-connect-3.1.32.pom
+mvn install:install-file -Dfile=lib/sauce-connect-3.1.32.jar -DpomFile=lib/sauce-connect-3.1.32.pom
 echo "SAUCE CONNECT INSTALLATION SUCCESSFUL!"
