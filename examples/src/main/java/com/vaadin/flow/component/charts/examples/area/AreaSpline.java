7ea74ca98abc68e0ef72f784f429a9499762e63c
Diego Cardoso <diego@vaadin.com>
2018-02-09 15:03:58 +0200

    Remove misleading comment

--- examples/src/main/java/com/vaadin/flow/component/charts/examples/area/AreaSpline.java
+++ examples/src/main/java/com/vaadin/flow/component/charts/examples/area/AreaSpline.java
@@ -42,7 +42,6 @@ public class AreaSpline extends AbstractChartExample {
 		XAxis xAxis = new XAxis();
 		xAxis.setCategories(new String[] { "Monday", "Tuesday", "Wednesday",
 				"Thursday", "Friday", "Saturday", "Sunday" });
-		// add blue background for the weekend
         PlotBand plotBand = new PlotBand(4.5, 6.5);
 		plotBand.setZIndex(1);
 		xAxis.setPlotBands(plotBand);
