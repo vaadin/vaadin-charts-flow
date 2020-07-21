3141d7141a201e329b6c202ee3b8089db9a6de33
Guillermo Alvarez <guillermo@vaadin.com>
2020-07-21 17:48:21 +0300

    Add missing styles
    Styles removed in #56 and #77 should've been added back in #351

--- addon/src/main/java/com/vaadin/flow/component/charts/model/style/AxisStyle.java
+++ addon/src/main/java/com/vaadin/flow/component/charts/model/style/AxisStyle.java
@@ -28,6 +28,10 @@ public class AxisStyle extends AbstractConfigurationObject {
     private TickIntervalStyle minorTickInterval;
     private Color lineColor;
     private Number lineWidth;
+    private Number tickWidth;
+    private Color tickColor;
+    private Color gridLineColor;
+    private Number gridLineWidth;
     private Color alternateGridColor;
 
     private final StyleWrapper title = new StyleWrapper();
@@ -51,7 +55,7 @@ public class AxisStyle extends AbstractConfigurationObject {
      * setting the minor tick interval to 1 puts one tick on each of 0.1, 1, 10,
      * 100 etc. Setting the minor tick interval to 0.1 produces 9 ticks between
      * 1 and 10, 10 and 100 etc. A minor tick interval of
-     * {@link TickIntervalStyle#AUTO} on a log axis results in a best guess,
+     * {@link TickIntervalStyle#AUTO}on a log axis results in a best guess,
      * attempting to enter approximately 5 minor ticks between each major tick.
      * Defaults to null.
      * 
@@ -92,6 +96,38 @@ public class AxisStyle extends AbstractConfigurationObject {
      */
     public void setLineWidth(Number lineWidth) {
         this.lineWidth = lineWidth;
+    }
+
+    /** 
+     * @see #setTickWidth(Number)
+     */ 
+    public Number getTickWidth() {
+        return tickWidth;
+    }
+
+    /** 
+     * Sets the pixel width of the major tick marks. Defaults to 1.
+     *
+     * @param tickWidth 
+     */ 
+    public void setTickWidth(Number tickWidth) {
+        this.tickWidth = tickWidth;
+    }
+
+    /** 
+     * @see #setTickColor(Color)
+     */ 
+    public Color getTickColor() {
+        return tickColor;
+    }
+
+    /** 
+     * Sets the color for the main tick marks. Defaults to #C0D0E0.
+     *
+     * @param tickColor 
+     */ 
+    public void setTickColor(Color tickColor) {
+        this.tickColor = tickColor;
     }
 
     /**
@@ -144,6 +180,25 @@ public class AxisStyle extends AbstractConfigurationObject {
         labels.setStyle(style);
     }
 
+    /** 
+     * @see #setGridLineWidth(Number)
+     *
+     * @return The width of grid lines or null if not defined
+     */ 
+    public Number getGridLineWidth() {
+        return gridLineWidth;
+    }
+
+    /** 
+     * Sets the width of the grid lines extending the ticks across the plot
+     * area. Defaults to 0.
+     *
+     * @param gridLineWidth
+     */ 
+    public void setGridLineWidth(Number gridLineWidth) {
+        this.gridLineWidth = gridLineWidth;
+    }
+
     /**
      * @see #setAlternateGridColor(Color)
      * 
@@ -164,4 +219,23 @@ public class AxisStyle extends AbstractConfigurationObject {
         this.alternateGridColor = alternateGridColor;
     }
 
+    /**
+     * @see #setGridLineColor(Color)
+     *
+     * @return The color of grid lines, null if not defined
+     */ 
+    public Color getGridLineColor() {
+        return gridLineColor;
+    }
+
+    /** 
+     * Sets the color of the grid lines extending the ticks across the plot
+     * area. Defaults to "#C0C0C0".
+     *
+     * @param gridLineColor
+     *            Color of grid lines
+     */ 
+    public void setGridLineColor(Color gridLineColor) {
+        this.gridLineColor = gridLineColor;
+    }
 }
